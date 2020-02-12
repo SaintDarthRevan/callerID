a43fd627592154b0bd618a41ffc29c4965a28a57
Sergey Sobyanin <49924330+SaintDarthRevan@users.noreply.github.com>
2020-02-12 15:25:19 +0500

    Update check_phone.php

--- check_phone.php
+++ check_phone.php
@@ -10,41 +10,41 @@ function identifyRegionByPhone($phone)
     if (!preg_match("/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/", $phone)) {
         $message = 'Некорректный формат номера';
     } else {
-		$phone = str_replace('+', '', $phone);
-		$phone = str_replace('-', '', $phone);
-		$phone = str_replace(' ', '', $phone);
-		$phone = str_replace('(', '', $phone);
-		$phone = str_replace(')', '', $phone);
+        $phone = str_replace('+', '', $phone);
+        $phone = str_replace('-', '', $phone);
+        $phone = str_replace(' ', '', $phone);
+        $phone = str_replace('(', '', $phone);
+        $phone = str_replace(')', '', $phone);
 
-		$code = $phone[1].$phone[2].$phone[3];
-		$number = '';
+        $code = $phone[1].$phone[2].$phone[3];
+        $number = '';
 
-		for($i = 4; $i <= 10; $i++) {
-			$number .= $phone[$i];
-		}
+        for($i = 4; $i <= 10; $i++) {
+            $number .= $phone[$i];
+        }
 
-		//var_dump($code, $number);
+        //var_dump($code, $number);
 
-		$DB = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-		$STH = $DB->prepare("SELECT * FROM `phone_codes` WHERE `def_code` = :code");
-		$STH->execute(['code' => $code]);
-		$region = '';
-		if ($STH->rowCount() > 0) {
-			while ($row = $STH->fetch()) {
-				if ( ((int)$row['numbers_range_f'] < (int)$number)
-					&& ((int)$row['numbers_range_t'] > (int)$number) ) {
-					$region = $row['region'];
-					break;
-				}
-			}
-		}
+        $DB = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+        $STH = $DB->prepare("SELECT * FROM `phone_codes` WHERE `def_code` = :code");
+        $STH->execute(['code' => $code]);
+        $region = '';
+        if ($STH->rowCount() > 0) {
+            while ($row = $STH->fetch()) {
+                if ( ((int)$row['numbers_range_f'] < (int)$number)
+                    && ((int)$row['numbers_range_t'] > (int)$number) ) {
+                    $region = $row['region'];
+                    break;
+                }
+            }
+        }
 
-		if ($region != '') {
-			$message = $region;
-		} else {
-			$message = 'Не удалось определить номер';
-		}
-	}
+        if ($region != '') {
+            $message = $region;
+        } else {
+            $message = 'Не удалось определить номер';
+        }
+    }
 	
     return $message;
 }
